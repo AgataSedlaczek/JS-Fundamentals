6c2f4a45447597792bb711d08e086b402af28264
AgataSedlaczek <navi2@o2.pl>
2023-06-09 12:59:02 +0200

    Initial commit

---
+++ lessons/lesson1.js
@@ -0,0 +1,18 @@
+// 1.Hello World
+console.log("Hello World!")
+
+// Variables
+
+var firstName = "Agata"
+let lastName = "Sedlaczek"
+console.log(firstName)
+
+var age, dateOfBirth, sex
+age = "5"
+sex = "Female"
+console.log(age)
+
+// constans
+
+const occupation = "Engineer"
+console.log(occupation)
